21d56b59eabc3139ee6ddb96ab415be5334d6f51
Alejandro Akifarry <alejandroakifarry@pop-os.localdomain>
2021-01-28 18:29:15 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,104 @@
+require("dotenv").config();
+const imaps = require("imap-simple");
+const Imap = require("imap");
+const path = require("path");
+const express = require("express");
+const inspect = require("util").inspect;
+const MailListener = require("mail-listener-next");
+const simpleParser = require("mailparser").simpleParser;
+
+// var config = {
+//   imap: {
+//     user: `${process.env.EMAIL_USER}`,
+//     password: `${process.env.EMAIL_PASS}`,
+//     host: `${process.env.EMAIL_HOST}`,
+//     port: 993,
+//     tls: true,
+//     authTimeout: 3000,
+//     tlsOptions: {
+//       rejectUnauthorized: false,
+//     },
+//   },
+// };
+
+var imap = new Imap({
+  user: `${process.env.EMAIL_USER}`,
+  password: `${process.env.EMAIL_PASS}`,
+  host: "imap.gmail.com",
+  port: 993,
+  tls: true,
+  tlsOptions: {
+    rejectUnauthorized: false,
+  },
+});
+
+function openInbox(cb) {
+  imap.openBox("INBOX", true, cb);
+}
+
+var emails = [];
+
+imap.once("ready", function () {
+  openInbox(function (err, box) {
+    if (err) throw err;
+    console.log(box.messages.total + " message(s) found!");
+    // 1:* - Retrieve all messages
+    // 3:5 - Retrieve messages #3,4,5
+    var f = imap.seq.fetch("1:*", {
+      bodies: "",
+    });
+    f.on("message", function (msg, seqno) {
+      console.log("Message #%d", seqno);
+      var prefix = "(#" + seqno + ") ";
+
+      msg.on("body", function (stream, info) {
+        // use a specialized mail parsing library (https://github.com/andris9/mailparser)
+        simpleParser(stream, (err, mail) => {
+          // console.log(prefix + mail.from.value[0].address);
+          // console.log(prefix + mail.subject);
+          // console.log(prefix + mail.text);
+          let newEmail = {
+            from: mail.from.value[0].address,
+            subject: mail.subject,
+            body: mail.text,
+          };
+          emails.push(newEmail);
+        });
+
+        // or, write to file
+        //stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
+      });
+      msg.once("attributes", function (attrs) {
+        console.log(prefix + "Attributes: %s", inspect(attrs, false, 8));
+      });
+      msg.once("end", function () {
+        console.log(prefix + "Finished");
+      });
+    });
+    f.once("error", function (err) {
+      console.log("Fetch error: " + err);
+    });
+    f.once("end", function () {
+      console.log("Done fetching all messages!");
+      imap.end();
+    });
+
+    // search example
+    //    imap.search([ 'UNSEEN', ['SINCE', 'May 20, 2010'] ], function(err, results) {
+    //      if (err) throw err;
+    //      var f = imap.fetch(results, { bodies: '' });
+    //      ...
+    //    }
+  });
+});
+
+imap.once("error", function (err) {
+  console.log(err);
+});
+
+imap.once("end", function () {
+  console.log("Connection ended");
+  console.log(emails);
+});
+
+imap.connect();
